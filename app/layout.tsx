3cadb8cc0aec03ed55e05b9be2922241e674746a
KrisiKostadinov <krisi.199898@gmail.com>
2025-02-22 03:20:17 +0200

    feat: refactor task management; remove unused loading components, enhance task columns with class and tutorial links, and update date formatting in task display

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ToastContainer } from "react-toastify";
-import { Suspense } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,13 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Suspense>
-          {children}
-          <ToastContainer position="top-center" />
-        </Suspense>
+      <body>
+        {children}
+        <ToastContainer position="top-center" />
       </body>
     </html>
   );
